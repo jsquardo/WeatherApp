ea70a68a141851697d0e585e4daaa1b2f8290791
Johnny Squardo <squ@rdo.host>
2019-03-01 17:42:36 -0500

    Almost done

---
+++ app.js
@@ -0,0 +1,37 @@
+window.addEventListener('load', () => {
+    let long;
+    let lat;
+    let temperatureDesciption = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationTimezone = document.querySelector('.location-timezone');
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const proxy = 'http://cors-anywhere.herokuapp.com/'
+            const api = `${proxy}https://api.darksky.net/forecast/c537faefd98a77579239c095845b8904/${lat},${long}`
+
+            fetch(api)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                const { temperature, summary, icon } = data.currently;
+                //Set DOM Elements from the API
+                temperatureDegree.textContent = temperature;
+                temperatureDesciption.textContent = summary;
+                locationTimezone.textContent = data.timezone;
+
+            })
+        });
+    }
+
+    function setIcons(icon, iconID) {
+        const skycons = new skycons({color: 'white'});
+        const currentIcon = icon.replace(/-/g, '_').toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
+    }
+});
